7133e3ecb7fc4bf546fa256f49ea752741a6c504
madhulikapathak4 <madhu2846@gmail.com>
2024-01-21 21:03:21 +0530

    Modified First File

--- first.java
+++ first.java
@@ -1,7 +1,16 @@
+import java.util.*;
 class first
 {
     public static void main(String args[])
     {
-        System.out.print("First");
+        Scanner s1=new Scanner(System.in);
+        String s;
+        System.out.print("are you hungry?");
+        s=s1.next();
+        if(s.equals("yes"))
+        System.out.print("Eat Samosa");
+        else
+        System.out.print("Do your homework");
+
     }
 }
